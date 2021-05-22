12bd16e3ae1d14c92797be721f3188d3fc7d4640
minjun_park <mjp@letsplax.com>
2021-05-22 16:51:44 +0900

    chagne to cloudflare CNAME :

--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
-let baseURL = `https://ec2-52-79-233-153.ap-northeast-2.compute.amazonaws.com:2083`;
+// let baseURL = `https://ec2-52-79-233-153.ap-northeast-2.compute.amazonaws.com:2083`;
+let baseURL = `https://api.todiary.ml:2083`;
  
 // let baseURL = `http://localhost:2083`
 
