f3ce03121fd5e63f06f12fd683078401659584d6
MINJUN PARK <mjp@letsplax.com>
2023-09-17 04:39:08 +0900

    change dbmanger

--- server/dbmanager.js
+++ server/dbmanager.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 const db = {};
-const uri = "mongodb+srv://minjun:4e@!fHNQCuG!VbG@cluster0.5qorl.mongodb.net/test";
+const uri = "mongodb+srv://bluebluerize900:qlalfqjsgh0@cluster0.t26mju5.mongodb.net/";
 const mongodb = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -109,135 +109,135 @@ app.post("/updateone",((req, res, next) => {
 
 //----------------------From IBM internship----------------//
 
-const express = require('express');
-const db = require('./dbManager.js');
-const app = express();
+// const express = require('express');
+// const db = require('./dbManager.js');
+// const app = express();
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Expose-Headers': '*',
-  'Access-Control-Allow-Methods': '*',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
+// const headers = {
+//   'Access-Control-Allow-Origin': '*',
+//   'Access-Control-Expose-Headers': '*',
+//   'Access-Control-Allow-Methods': '*',
+//   'Access-Control-Allow-Headers': 'Content-Type',
+// };
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json({ limit: '5mb' }));
-app.use((req, res, next) => { res.set(headers); next(); })
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json({ limit: '5mb' }));
+// app.use((req, res, next) => { res.set(headers); next(); })
 
-// const port = process.env.PORT || 8080;
-const port = 8080;
+// // const port = process.env.PORT || 8080;
+// const port = 8080;
 
-app.get("/user/email", async (req, res) => {
-  const email = req.query.email;
-  const userInfo =
-    await db.findOne("user", "info", { email })
-      .catch((e) => {
-        console.log(e)
-        res.sendStatus(404);
-      });
-  res.send(userInfo);
-});
+// app.get("/user/email", async (req, res) => {
+//   const email = req.query.email;
+//   const userInfo =
+//     await db.findOne("user", "info", { email })
+//       .catch((e) => {
+//         console.log(e)
+//         res.sendStatus(404);
+//       });
+//   res.send(userInfo);
+// });
 
-app.get("/test", async (req, res) => {
-  res.send("yes");
-})
+// app.get("/test", async (req, res) => {
+//   res.send("yes");
+// })
 
 
-//date, start time, title
-//@param date : format => 2021-12-10T13:00
-app.post("/schedule", async (req, res) => {
-  let { email, startDate, title, memo, endDate } = req.body;
-  if (email === undefined || email === null) email = "test_user"
-  // console.log(req.body);
-  const [year, month, day, time] = startDate.split(/[-T]/);
-  const _id =
-    await db.writeOne("user", "schedule", {
-      title,
-      memo,
-      startDate,
-      endDate: endDate ? endDate : startDate,
-      email,
-    })
-      .catch((e) => {
-        console.log(e);
-        res.sendStatus(404);
-      });
+// //date, start time, title
+// //@param date : format => 2021-12-10T13:00
+// app.post("/schedule", async (req, res) => {
+//   let { email, startDate, title, memo, endDate } = req.body;
+//   if (email === undefined || email === null) email = "test_user"
+//   // console.log(req.body);
+//   const [year, month, day, time] = startDate.split(/[-T]/);
+//   const _id =
+//     await db.writeOne("user", "schedule", {
+//       title,
+//       memo,
+//       startDate,
+//       endDate: endDate ? endDate : startDate,
+//       email,
+//     })
+//       .catch((e) => {
+//         console.log(e);
+//         res.sendStatus(404);
+//       });
 
-  const $set = {};
-  $set[`scheduleIds.${year}.${month}.${day}.${time}`] = _id;
+//   const $set = {};
+//   $set[`scheduleIds.${year}.${month}.${day}.${time}`] = _id;
 
-  db.updateOne("user", "info", { email }, { $set }, { upsert: true })
-    .then(() => {
-      console.log("insert complete! " + _id);
-      res.sendStatus(200);
-    })
-    .catch((e) => {
-      console.log(e);
-      res.sendStatus(404);
-    });
-});
+//   db.updateOne("user", "info", { email }, { $set }, { upsert: true })
+//     .then(() => {
+//       console.log("insert complete! " + _id);
+//       res.sendStatus(200);
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       res.sendStatus(404);
+//     });
+// });
 
-app.get("/schedule", async (req, res) => { //
-  // const data;
-  let { email, date, searchmode } = req.query;
-  console.log(req.query);
-  if (email === undefined || email === null) email = "test_user"
-  if (date === undefined || date === null) {
-    res.sendStatus(401);
-    return;
-  }
-  // const [year, month, day, time] = date.split(/[-T]/);
-  const [year, month, day] = date.split(/[-T]/);
-  const projection = {};
-  if (searchmode == 'month') {
-    projection[`scheduleIds.${year}.${month}`] = 1;
-  } else if (searchmode == 'day') {
-    projection[`scheduleIds.${year}.${month}.${day}`] = 1;
-  } else { // searchmode == time
-    // search with specific time??? 
-    //temp
-    res.sendStatus(404);
-    return;
-  }
+// app.get("/schedule", async (req, res) => { //
+//   // const data;
+//   let { email, date, searchmode } = req.query;
+//   console.log(req.query);
+//   if (email === undefined || email === null) email = "test_user"
+//   if (date === undefined || date === null) {
+//     res.sendStatus(401);
+//     return;
+//   }
+//   // const [year, month, day, time] = date.split(/[-T]/);
+//   const [year, month, day] = date.split(/[-T]/);
+//   const projection = {};
+//   if (searchmode == 'month') {
+//     projection[`scheduleIds.${year}.${month}`] = 1;
+//   } else if (searchmode == 'day') {
+//     projection[`scheduleIds.${year}.${month}.${day}`] = 1;
+//   } else { // searchmode == time
+//     // search with specific time??? 
+//     //temp
+//     res.sendStatus(404);
+//     return;
+//   }
 
-  const data =
-    (await db.findOne("user", "info", { email }, { projection })
-      .catch((e) => {
-        console.log(e);
-        res.sendStatus(404);
-        return;
-      })).scheduleIds;
-  console.log(data);
+//   const data =
+//     (await db.findOne("user", "info", { email }, { projection })
+//       .catch((e) => {
+//         console.log(e);
+//         res.sendStatus(404);
+//         return;
+//       })).scheduleIds;
+//   console.log(data);
 
-  const searchFilters = []; //to search scheduleIds
+//   const searchFilters = []; //to search scheduleIds
 
-  if (searchmode == 'month') {
-    const days = data[year][month];
-    for (const [key, obj] of Object.entries(days)) {
-      const scheduleIds = Object.values(obj);
-      for (const schedule_id of Object.values(obj)) searchFilters.push({ _id: schedule_id })
-      console.log(scheduleIds);
-      console.log('---------');
-    }
-  } else if (searchmode == 'day') {
-    for (const schedule_id of Object.values(data[year][month][day]))
-      searchFilters.push({ _id: schedule_id })
-  }
+//   if (searchmode == 'month') {
+//     const days = data[year][month];
+//     for (const [key, obj] of Object.entries(days)) {
+//       const scheduleIds = Object.values(obj);
+//       for (const schedule_id of Object.values(obj)) searchFilters.push({ _id: schedule_id })
+//       console.log(scheduleIds);
+//       console.log('---------');
+//     }
+//   } else if (searchmode == 'day') {
+//     for (const schedule_id of Object.values(data[year][month][day]))
+//       searchFilters.push({ _id: schedule_id })
+//   }
 
-  db.findAll("user", "schedule", searchFilters)
-    .then((result) => {
-      console.log('result!!!!');
-      console.log(result);
-      res.send(result);
-      return;
-    })
-    .catch((e) => {
-      console.log(e);
-      res.sendStatus(401)
-    });
-})
+//   db.findAll("user", "schedule", searchFilters)
+//     .then((result) => {
+//       console.log('result!!!!');
+//       console.log(result);
+//       res.send(result);
+//       return;
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       res.sendStatus(401)
+//     });
+// })
 
-//-------------------Local function for test------------//
+// //-------------------Local function for test------------//
 
 const getTest = async (email) => {
   const data = await db.findOne("user", "info", { email })
@@ -254,12 +254,12 @@ app.listen(port, '0.0.0.0', async () => {
   getTest("abc@email.com");
 });
 
-app.listen(port, "0.0.0.0", ()=>{ 
-	console.log("started db server");
-	mongodb.connect(function(err) {
-		if(!err) console.log("mongodb connected");
-		else console.log(err);
-		db["userdata"] = mongodb.db("userdata");
-		db["content"] = mongodb.db("content");
-	})
-});
+// app.listen(port, "0.0.0.0", ()=>{ 
+// 	console.log("started db server");
+// 	mongodb.connect(function(err) {
+// 		if(!err) console.log("mongodb connected");
+// 		else console.log(err);
+// 		db["userdata"] = mongodb.db("userdata");
+// 		db["content"] = mongodb.db("content");
+// 	})
+// });
